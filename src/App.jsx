0637c6d6865faa47af80d5ea8ea52c8d38d6ba0d
Diyorbek <tursunovdiyorbek570@gmail.com>
2023-07-29 21:23:18 +0500

    Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
+import { Route , Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import LogIn from "./pages/LogIn"
+import Register from "./pages/Register"
 function App() {
-
   return (
     <>
-      
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/login" element={<LogIn/>}/>
+        <Route path="/Register" element={<Register/>}/>
+      </Routes>
     </>
   )
 }
